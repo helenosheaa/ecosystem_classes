9ebc242623ec05b7ed709067e709e392e958719c
helenosheaa <helenosheaa@gmail.com>
2018-05-29 16:31:53 +0100

    river name test passed

---
+++ specs/river_spec.rb
@@ -0,0 +1,14 @@
+require('minitest/autorun')
+require_relative('../river.rb')
+
+class RiverTest < MiniTest::Test
+
+  def setup()
+    @river = River.new("Amazon")
+  end
+
+  def test_river_name()
+    assert_equal("Amazon", @river.name())
+  end
+
+end
